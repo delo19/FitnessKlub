46c051dc1ee43486015024cda092bd89c99a90e2
msulima <mateusz.sulima0@gmail.com>
2013-10-05 12:37:00 +0200

    Initial commit. Zrobilem template na jquery

---
+++ js/include.js
@@ -0,0 +1,17 @@
+/*
+This file is for including  parts from template sources
+*/
+
+
+$(document).ready(function () {
+
+    $('body').load('template/nav.html');
+    $('body').load('template/rightNav.html');
+    $('body').load('template/feftNav.html');
+    $('body').load('template/header.html');
+    $('body').load('template/footer.html');
+
+
+
+});
+
