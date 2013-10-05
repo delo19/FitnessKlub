75c4587630493abec66c7b17571e32dc5fdad51e
msulima <mateusz.sulima0@gmail.com>
2013-10-05 15:06:38 +0200

    Dodatki do templata

--- js/include.js
+++ js/include.js
@@ -6,14 +6,21 @@ This file is for including  parts from template sources
 $(document).ready(function () {
 
 
-    jQuery.ajaxSetup({ async: false }); 
-    $.get('template/nav.html', '', function (data) { $('body').append(data); });
-    $.get('template/rightNav.html', '', function (data) { $('body').append(data); });
-    $.get('template/leftNav.html', '', function (data) { $('body').append(data); });
-    $.get('template/header.html', '', function (data) { $('body').append(data); });
-    $.get('template/footer.html', '', function (data) { $('body').append(data); });
-
-
+    jQuery.ajaxSetup({ async: false });
+    $.get('template/header.html', '', function (data) { $("#page").append(data); });
+    $.get('template/nav.html', '', function (data) { $("#page").append(data); });
+    $.get('template/leftNav.html', '', function (data) { $("#pageContent").append(data); });
+    $.get('template/rightNav.html', '', function (data) { $("#pageContent").append(data); });
+    $.get('template/footer.html', '', function (data) { $("#page").append(data); });
+    reorderElements();
 
 });
 
+function reorderElements(){
+        //$('#rightNav').insertBefore('#footer');
+        $('#nav').insertBefore('#pageContent');
+
+        $('#leftNav').insertBefore('#content');
+        //$('#nav').insertBefore('#leftNav');
+        $('#header').insertBefore('#nav');
+}
