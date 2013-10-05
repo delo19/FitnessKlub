dc66553d7cb64b2167007168b026075d4ad540de
msulima <mateusz.sulima0@gmail.com>
2013-10-05 13:24:30 +0200

    Poprawka do loadera templata

--- js/include.js
+++ js/include.js
@@ -5,11 +5,13 @@ This file is for including  parts from template sources
 
 $(document).ready(function () {
 
-    $('body').load('template/nav.html');
-    $('body').load('template/rightNav.html');
-    $('body').load('template/feftNav.html');
-    $('body').load('template/header.html');
-    $('body').load('template/footer.html');
+
+    jQuery.ajaxSetup({ async: false }); 
+    $.get('template/nav.html', '', function (data) { $('body').append(data); });
+    $.get('template/rightNav.html', '', function (data) { $('body').append(data); });
+    $.get('template/leftNav.html', '', function (data) { $('body').append(data); });
+    $.get('template/header.html', '', function (data) { $('body').append(data); });
+    $.get('template/footer.html', '', function (data) { $('body').append(data); });
 
 
 
