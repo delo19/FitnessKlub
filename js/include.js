5fed1b2d005bf38512c960a17988df5adf3528fb
msulima <mateusz.sulima0@gmail.com>
2013-10-09 18:45:41 +0200

    Pare zmian

--- js/include.js
+++ js/include.js
@@ -18,7 +18,7 @@ function includeParts() {
     $.get('template/nav.html', '', function (data) { $(data).insertBefore("#pageContent"); });
     $.get('template/leftNav.html', '', function (data) {  $(data).insertBefore("#content"); });
     $.get('template/rightNav.html', '', function (data) { $(data).insertAfter("#content");});
-    $.get('template/footer.html', '', function (data) { $("#page").append(data); });
+    $.get('template/footer.html', '', function (data) { $('body').append(data); });
 
 }
 
