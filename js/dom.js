82862a88a6642c1b12d29a581c28238c72e6d666
mI3k <semla.michal@g.pl>
2013-11-18 21:43:43 +0100

    added 4 from DOM

--- js/dom.js
+++ js/dom.js
@@ -46,3 +46,75 @@ function update6()
 	var item=document.getElementsByTagName('p')[0];	
 	item.parentNode.removeChild(item);
 }
+
+var color   = 'red'; 
+var columns = 25;    
+var rows    = 25;   
+var mouse   = 0;     
+ 
+/**/
+
+window.onload = function ()
+{
+    table_create(); 
+    table_events();
+    document.body.ondragstart = function(e){return false}   
+    document.getElementById('drawing_table').onmousedown = mousedown;
+    document.getElementById('drawing_table').onmouseup   = function(e){mouse = 0}
+}
+ 
+
+function mousedown(e)
+{ 
+    var evt = e || window.event;
+	 if ( e.ctrlKey ) {
+		color = 'blue';
+		}
+	if ( e.shiftKey ) {
+		color = 'red';
+		}
+
+		if ( e.altKey ) {
+		color = 'white';
+		}
+  
+    if (evt.preventDefault) e.preventDefault();   
+    if (evt.which) mouse = evt.which;
+    else           mouse = evt.button;  
+    if (this.tagName == 'TD' && mouse == 1) this.style.backgroundColor = color;
+}
+ 
+
+function table_create()
+{
+    var tbl = ''; 
+   
+    for (var i=0; i<rows; i++){
+        tbl = tbl + '<tr>';
+        for (var j=0; j<columns; j++)    tbl = tbl + '<td width = 10px height=15px></td>';
+        tbl = tbl + '</tr>\n';
+    }
+  
+    var div = document.getElementById('drawing_table');
+    div.innerHTML = '<table cellspacing="0" cellpadding="0">' + tbl + '</table>';
+}
+ 
+
+function table_events()
+{
+    var td = document.getElementById('drawing_table').getElementsByTagName('td');
+   
+    for (var i=0; i<td.length; i++){
+        td[i].onmousedown = mousedown;
+     
+        td[i].onmouseover = function (e){if (mouse == 1) this.style.backgroundColor = color}
+    }
+}
+
+function set_color(obj)
+{
+    color = obj.style.backgroundColor;
+    var tbl = document.getElementById('drawing_table').firstChild;
+    tbl.style.borderColor = color;
+}
+
