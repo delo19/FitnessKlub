51206cb8b0910f6659a39bf67e6444a236ab4546
mI3k <semla.michal@g.pl>
2013-12-04 23:32:35 +0100

    working regexp, fixed bug with regexps

--- contact.php
+++ contact.php
@@ -2,27 +2,28 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <!--Script region-->
         <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.0.min.js"></script><!--JQUERY-->
         <script type="text/javascript" src="js/include.js"></script><!--Include fragments-->
         <script type="text/javascript" src="js/customScripts.js"></script><!--Email sender-->
         <script type="text/javascript" src="js/dom.js"></script>
         <link href="css/contact.css" rel="stylesheet" />
         <title>Contact</title>
-
-
-
     </head>
     <body>
-
-        <!--
-        Here we have all page content downloaded using include.js
-        You can add page content using section "content"
-        -->
         <div id="page">
             <section id="pageContent">
                 <article id="content">
-                    <form onsubmit="contact.php" onreset="resetAlert()">
+                    <?php 
+                        session_start();
+                        if( !isset($_SESSION['ta']) ){
+                            $_SESSION['ta'] = array();
+                        }
+
+                        if( $_SERVER['REQUEST_METHOD'] == 'POST'  && strlen($_POST['ta']) > 0 ){
+                            $_SESSION['ta'] = checkWords();
+                        }
+                    ?>
+                    <form method="POST" onreset="resetAlert()">
                         <p>Subskrybcja mailowa <input type="checkbox" name="nazwa" value="subscription" /></p>
                         <p>Pleć: K<input type="radio" name="sex" value="male" /> M<input type="radio" name="sex" value="female" /></p>
                         <p>Imie: <input type="text" name="Name" /></p>
@@ -40,61 +41,21 @@
 
                     <ul>
                         <?php foreach($_SESSION['ta'] AS $text): ?>
-                            <li><?php echo $text; ?></li>
+                            <li><?php echo "$text"; ?></li>
                         <?php endforeach; ?>
                     </ul>
                     <?php
-                        $isFocused = false;
-
-                        function setFocus() { 
-                            global $isFocused;
-                            $isFocused = true;
-                        }
-
-                        function getTextAreaContent() {
-                            return $_POST['ta'];
-                        }
-
-                        function getArrayOfWords() {
-                            return explode(' ', getTextAreaContent());
-                        }
-
                         function checkWords() {
-                            $words = getArrayOfWords();
-                            echo $words;
+                            $words = explode(' ', $_POST['ta']);
+                            $i = 0;
                             foreach ($words as $word) {
-                                echo "$word";
-                                checkWord("$word");
+                                if( preg_match('/invalid/', $word) ) {
+                                    preg_replace('/invalid/', 'valid', $word);
+                                    $illegalWords[$i] = $word;
+                                    $i++;
+                                }
                             }
-                        }
-
-                        function checkWord( $word ){
-                            echo "$word";
-                            if( preg_match('/invalid/', $word) ) {
-                                echo "<p> omg </p>";
-                                $_SESSION['ta'][] = $word;
-                                preg_replace('/invalid/', 'valid', $word);
-                            }
-                        }
-
-                        function checkIfInvalid() {
-                            global $isFocused;
-                            if ( $isFocused ) {
-                                checkWords();
-                                $isFocused = false;
-                            }
-                        }
-
-                        session_start();
-                        if( !isset($_SESSION['ta']) ){
-                            // set session array if not already set previously
-                            $_SESSION['ta'] = array();
-                        }
-
-                        if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
-                            // add text to session array and escape for security
-                            //$_SESSION['ta'][] = htmlspecialchars($_POST['ta'], ENT_QUOTES);
-                            checkIfInvalid();
+                            return $illegalWords;
                         }
                     ?>
                 </article>
